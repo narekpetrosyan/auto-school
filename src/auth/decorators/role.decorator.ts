c7d87663b45afe7a415d936418a45780c3c42aac
narekpetrosyan <narek.petrosyan988@gmail.com>
2025-02-06 15:05:20 +0400

    Add admin user seed

--- src/auth/decorators/role.decorator.ts
+++ src/auth/decorators/role.decorator.ts
@@ -2,4 +2,5 @@ import { SetMetadata } from '@nestjs/common';
 import { RoleTypes } from '../types/role';
 
 export const ROLE_KEY = 'role';
-export const Role = (...roles: RoleTypes[]) => SetMetadata(ROLE_KEY, roles);
+export const Role = (...roles: `${RoleTypes}`[]) =>
+  SetMetadata(ROLE_KEY, roles);
---
+++ src/seed/user.seed.ts
@@ -0,0 +1,25 @@
+import { PrismaClient } from '@prisma/client';
+import * as bcrypt from 'bcryptjs';
+import { RoleTypes } from '../auth/types/role';
+
+const prisma = new PrismaClient();
+
+const ADMIN_USER = {
+  firstName: 'Admin',
+  lastName: 'User',
+  email: 'admin@email.com',
+  password: bcrypt.hashSync('password', 10),
+  role: RoleTypes.ADMIN,
+};
+
+async function main() {
+  // config();
+  console.log('Seeding admin user...');
+  await prisma.user.create({ data: ADMIN_USER });
+}
+
+main()
+  .catch((e) => console.error(e))
+  .finally(() => {
+    void prisma.$disconnect();
+  });
